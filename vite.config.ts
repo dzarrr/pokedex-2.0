3168b0e0f01bef7b192f7fa9e6959fc00fb6688a
dzarrr <dzarbelahanifa@gmail.com>
2024-04-28 19:29:27 +0700

    feat: update github page deployment config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/pokedex-2.0/",
   server: {
     port: 3000,
   },
